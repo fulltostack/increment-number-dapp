aef09ccdcfbde95ce54dfab923f0defd535aaee8
fullToStack <fulltostack@gmail.com>
2020-03-03 18:08:33 +0530

    Return balance as ethers instead of wei

--- src/utils/metamask.js
+++ src/utils/metamask.js
@@ -1,73 +1,38 @@
 const Web3 = require('web3');
 
+export const CHROME_INSTALLATION = "https://www.google.com/chrome/browser/desktop/"
+export const METAMASK_CHROME_EXTENSTION = "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en"
+
 export const isMetamaskInstalled = () => {
   return !!window.ethereum || !!window.web3;
 }
 
 export const initializeWeb3 = async () => {
   if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-        try {
-          // Request account access if needed
-          console.log('-----Initialising-----');
-          await window.ethereum.enable();
-          console.log('-----Done-----');
-          // Acccounts now exposed
+    window.web3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+      // Acccounts now exposed
 
-        } catch (error) {
-          // User denied account access...
-        }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        window.web3 = new Web3(window.web3.currentProvider);
-        // Acccounts always exposed
-      }
-      // Non-dapp browsers...
-      else {
-        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-      }
+    } catch (error) {
+      throw new Error("User Denied Access")
+    }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+    // Acccounts always exposed
+  }
+  // Non-dapp browsers...
+  else {
+    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+  }
 }
 
 export const getAccounts = async () => window.web3.eth.getAccounts();
 
-export const getBalance = async address => window.web3.eth.getBalance(address);
-
-// window.addEventListener('load', async () => {
-//   // Modern dapp browsers...
-//   if (window.ethereum) {
-//     window.web3 = new Web3(ethereum);
-//     try {
-//       // Request account access if needed
-//       await ethereum.enable();
-//       // Acccounts now exposed
-//     } catch (error) {
-//       // User denied account access...
-//     }
-//   }
-//   // Legacy dapp browsers...
-//   else if (window.web3) {
-//     window.web3 = new Web3(web3.currentProvider);
-//     // Acccounts always exposed
-//   }
-//   // Non-dapp browsers...
-//   else {
-//     console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-//   }
-// });
-
-
-
-// export const isUserLoggedIn = () => {
-//   web3.eth.getAccounts(function(err, accounts){
-//     if (err != null) {
-//        console.log(err)
-//     }
-//     else if (accounts.length === 0) {
-//        console.log('MetaMask is locked')
-//     }
-//     else {
-//        console.log('MetaMask is unlocked')
-//     }
-//  });
-// }
+export const getBalance = async address => {
+  const result = await window.web3.eth.getBalance(address)
+  return Number(window.web3.utils.fromWei(result, 'ether'))
+};
