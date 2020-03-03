07c3d87f65e47358c0c34bd709557057d970877a
fullToStack <fulltostack@gmail.com>
2020-03-03 18:07:44 +0530

    Add connected react router

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
 import metamask from '../containers/Startup/reducer';
 import user from '../containers/User/reducer';
+import { connectRouter } from 'connected-react-router'
 
-export default combineReducers({ metamask, user });
+const createRootReducer = (history) => combineReducers({
+  router: connectRouter(history),
+  metamask,
+  user,
+})
+export default createRootReducer
