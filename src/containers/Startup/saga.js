445331a41a78ca1eac475446aa3eab8fa9c4e469
fullToStack <fulltostack@gmail.com>
2020-03-03 18:13:18 +0530

    Check if metamask is not installed

--- src/containers/Startup/saga.js
+++ src/containers/Startup/saga.js
@@ -1,13 +1,13 @@
 import { call, put } from 'redux-saga/effects';
 import { isMetamaskInstalled, initializeWeb3 } from '../../utils/metamask';
-
 import { startUpdateAccount } from '../User/reducer';
+import { history } from '../../app/store'
 
 function* startupSaga(action) {
    try {
       console.log('Starting UP: --');
       const isInstalled = isMetamaskInstalled();
-      if(isInstalled) {
+      if (isInstalled) {
          // yield call(initializeWeb3);
          console.log('--Startinf Initialisation');
          yield call(initializeWeb3);
@@ -15,6 +15,8 @@ function* startupSaga(action) {
          yield put(startUpdateAccount())
          // const accounts = yield call(getAccounts);
          // console.log(accounts);
+      } else {
+         history.push('/no-metamask')
       }
    } catch (e) {
    }
